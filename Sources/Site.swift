3cc731ad6360942a40014fdc04391fd1c7fee6b1
mtj0928 <mtj0928@users.noreply.github.com>
2024-09-25 21:52:31 +0900

    Fix URL

--- Sources/Site.swift
+++ Sources/Site.swift
@@ -17,7 +17,7 @@ struct IgniteWebsite {
 struct ExampleSite: Site {
     var name = "Hello World"
     var titleSuffix = " – My Awesome Site"
-    var url = URL(string: "https://www.example.com")!
+    var url = URL(string: "https://mtj0928.github.io/sample-ignite/")!
     var builtInIconsEnabled = true
 
     var author = "John Appleseed"
