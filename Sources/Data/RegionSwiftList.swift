23de40469bbbbb7d2c8ab36a32576907c251ccee
yugo.sugiyama <sugiy.u.2203@gmail.com>
2024-09-27 15:38:26 +0900

    Add Hakata.swift

--- Sources/Data/RegionSwiftList.swift
+++ Sources/Data/RegionSwiftList.swift
@@ -2,6 +2,7 @@ import Foundation
 
 enum RegionSwiftList {
     static let regions: [RegionSwift] = [
+        RegionSwift(name: "Hakata.swift", imageFilePath: "/images/regions/hakataswift.png", link: URL(string: "https://hakata-swift.connpass.com")!),
         RegionSwift(name: "Kamiyama.swift", imageFilePath: "/images/regions/kamiyamaswift.png", link: URL(string: "https://kamiyama-swift.connpass.com")!),
         RegionSwift(name: "Chiba.swift", imageFilePath: "/images/regions/chibaswift.png", link: URL(string: "https://chibaswift.connpass.com")!),
         RegionSwift(name: "Kanagawa.swift", imageFilePath: "/images/regions/kanagawa.png", link: URL(string: "https://kanagawaswift.connpass.com")!),
